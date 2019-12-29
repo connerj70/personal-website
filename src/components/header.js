f9f107e4f77f50d5dba2b2769c0a18150bb3b831
Conner Jensen <connerj70@gmail.com>
2019-12-29 09:11:07 -0700

    first commit

--- src/components/header.js
+++ src/components/header.js
@@ -1,35 +1,43 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
-import React from "react"
+import React, { useState } from "react"
+import { FaBars } from "react-icons/fa"
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
+const Header = ({ siteTitle }) => {
+  const [showHeader, setShowHeader] = useState(true)
+  const headerLinks = ["about", "skills", "portfolio", "contact"]
+  return (
+    <header className="text-white">
+      <div
+        className={
+          "fixed w-10 h-10 bg-black flex justify-center items-center rounded-r" +
+          (showHeader ? " invisible" : " visible")
+        }
+        onClick={() => setShowHeader(true)}
+      >
+        <FaBars color="#fff" />
+      </div>
+      <div
+        className={
+          "fixed w-3/4 md:w-1/4 lg:1/6 h-screen bg-black lg:visible" +
+          (showHeader ? " visible" : " invisible")
+        }
+      >
+        <span
+          className="fixed ml-4 mt-4 lg:invisible"
+          onClick={() => setShowHeader(false)}
         >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
-)
+          X
+        </span>
+        <ul className="p-10 mt-5">
+          {headerLinks.map(item => {
+            return <li className="cursor-pointer">{item}</li>
+          })}
+        </ul>
+      </div>
+    </header>
+  )
+}
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
