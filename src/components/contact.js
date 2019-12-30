ff890088d1ebacec506f31dffb4ee5616a08b1ce
Conner Jensen <connerj70@gmail.com>
2019-12-29 18:26:02 -0700

    g

--- src/components/contact.js
+++ src/components/contact.js
@@ -12,8 +12,7 @@ const Contact = () => {
         className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
         name="contact2"
         method="POST"
-        netlify
-        action="/"
+        netlify="true"
       >
         <input type="hidden" name="form-name" value="contact2" />
         <p>
