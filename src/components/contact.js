ac404ec78c7644fb43c1a565713bd2b0a272fa78
Conner Jensen <connerj70@gmail.com>
2019-12-29 18:28:27 -0700

    g

--- src/components/contact.js
+++ src/components/contact.js
@@ -13,8 +13,12 @@ const Contact = () => {
         name="contact2"
         method="POST"
         netlify="true"
+        netlify-honeypot="bot-field"
       >
         <input type="hidden" name="form-name" value="contact2" />
+        <p class="hidden">
+          <label>Don’t fill this out if you're human: <input name="bot-field" /></label>
+        </p>
         <p>
           <label className="block text-gray-700 text-sm font-bold mb-2">
             Your Name: <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="text" name="name" />
