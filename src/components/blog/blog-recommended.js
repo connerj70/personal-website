47348c928fa6b546f892fd588f2a46c9330d86fe
Conner Jensen <connerj70@gmail.com>
2020-07-11 09:09:01 -0600

    Change recommended posts color

--- src/components/blog/blog-recommended.js
+++ src/components/blog/blog-recommended.js
@@ -6,24 +6,24 @@ const BlogRecommended = () => {
       style={{ top: "630px", maxWidth: "500px", margin: "0 auto" }}
       className="p-2 md:sticky"
     >
-      <h2 className="text-xl font-semibold bg-gray-300 p-2 pt-3 pb-3">
-        ⭐ Recommended Posts
+      <h2 className="text-xl font-semibold bg-gray-700 p-2 pt-3 pb-3 text-white">
+        ⭐ <span className="ml-2">Recommended Posts</span>
       </h2>
       <ul>
         <a href="/blog/top-programming-languages-2020">
-          <li className="p-3 border-b-4 transition duration-300 hover:bg-c-orange">
+          <li className="p-3 border-b-4 transition duration-300 hover:bg-gray-300">
             {" "}
             Top Programming Languages 2020
           </li>
         </a>
         <a href="/blog/highest-paying-programming-jobs">
-          <li className="p-3 border-b-4 transition duration-300 hover:bg-c-orange">
+          <li className="p-3 border-b-4 transition duration-300 hover:bg-gray-300">
             {" "}
             Highest Paying Programming Jobs
           </li>
         </a>
         <a href="/blog/concurrency-patterns-runner">
-          <li className="p-3 border-b-4 transition duration-300 hover:bg-c-orange">
+          <li className="p-3 border-b-4 transition duration-300 hover:bg-gray-300">
             Concurrency Patterns In Go
           </li>
         </a>
